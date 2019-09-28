dad33a99e631c5d5202294e41a627224c6aafd46
085astatine <astatine085@gmail.com>
2019-09-28 11:17:31 +0900

    Add clean script

---
+++ clean.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pathlib
+import yaml
+
+
+def main():
+    config_path = pathlib.Path('config.yaml')
+    with config_path.open() as config_file:
+        config = yaml.load(
+                config_file,
+                Loader=yaml.SafeLoader)
+
+    for target in config['target'].values():
+        directory = pathlib.Path(target['save_directory'])
+        if directory.exists():
+            for mail_file in directory.iterdir():
+                mail_file.unlink()
+            directory.rmdir()
+
+    target_list = [
+            'bookwalker.md',
+            'bookwalker.csv',
+            'yodobashi.md',
+            'yodobashi.csv']
+    for filename in target_list:
+        path = pathlib.Path(filename)
+        if path.exists():
+            path.unlink()
+
+
+if __name__ == '__main__':
+    main()
