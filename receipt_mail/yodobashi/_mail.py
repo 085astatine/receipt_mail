5e8ff35779385be949eeec78e499ac4ddb9e3fcc
085astatine <astatine085@gmail.com>
2020-08-03 14:10:30 +0900

    Add logger output in yodobashi

--- receipt_mail/yodobashi/_mail.py
+++ receipt_mail/yodobashi/_mail.py
@@ -2,6 +2,7 @@
 
 import datetime
 import re
+import textwrap
 from typing import List, NamedTuple, Optional, Tuple
 from .._mail import Mail as MailBase
 
@@ -29,18 +30,30 @@ class Mail(MailBase):
 
     def receipt(self) -> List[Receipt]:
         result: List[Receipt] = []
-        text = self.text_list()[0]
-        order = _extract_item_list(text)
-        if order:
-            item_list = _item_list(order)
-            shipping = _shipping(order)
-            granted_point = _granted_point(text)
-            receipt = Receipt(
-                    items=tuple(item_list),
-                    shipping=shipping,
-                    granted_point=granted_point,
-                    purchased_date=self.date())
-            result.append(receipt)
+        self.logger.debug(
+            'structure:\n%s',
+            textwrap.indent(self.structure(), '    '))
+        for i, text in enumerate(self.text_list()):
+            self.logger.debug('text %d:\n%s', i, textwrap.indent(text, '    '))
+            order = _extract_item_list(text)
+            if order:
+                self.logger.debug('order:\n%s', textwrap.indent(order, '    '))
+                # item list
+                item_list = _item_list(order)
+                self.logger.debug('item list: %s', item_list)
+                # shipping
+                shipping = _shipping(order)
+                self.logger.debug('shipping: %d', shipping)
+                # granted point
+                granted_point = _granted_point(text)
+                self.logger.debug('granted point: %d', granted_point)
+                # receipt
+                receipt = Receipt(
+                        items=tuple(item_list),
+                        shipping=shipping,
+                        granted_point=granted_point,
+                        purchased_date=self.date())
+                result.append(receipt)
         return result
 
 
